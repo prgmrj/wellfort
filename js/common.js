56f5d647f2a9e2705000f15bc45330abd178e3b8
prgmrj <prgmrj@gmail.com>
2019-06-24 17:59:18 +0900

    .

--- js/common.js
+++ js/common.js
@@ -1,8 +1,17 @@
-$(document).on('mouseenter mouseleave', '#gnb', menuopen);
+$(document)
+	.on('mouseenter mouseleave', '.gnb', menuopen)
+	.on('scroll', gnbstyle);
 
 function menuopen(e){
 	if (e.type == 'mouseenter')
 		$('header').addClass('_opened');
 	else
 		$('header').removeClass('_opened');
+}
+
+function gnbstyle(){
+	if ($(document).scrollTop() > 10)
+		$('header').addClass('_scrolled');
+	else
+		$('header').removeClass('_scrolled');
 }
