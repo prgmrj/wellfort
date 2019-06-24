2da1bd1489dfb3ca0b34e742b3e26e2beb8166b0
prgmrj <prgmrj@gmail.com>
2019-06-24 16:16:45 +0900

    .

--- js/common.js
+++ js/common.js
@@ -1,13 +1,8 @@
-$(document).on('mouseenter mouseleave', '#gnb .dep1 > li', gnbRoll);
+$(document).on('mouseenter mouseleave', '#gnb', menuopen);
 
-function gnbRoll(e){
-	if (e.type == 'mouseenter'){
-		$(this).find('hr').stop().animate({ width: '100%', left: 0 }, 200);
-		$('.dep2').stop().animate({ height: 330 }, 200);
-		$('#menubg').stop().animate({ height: 330 }, 200);
-	} else{
-		$(this).find('hr').stop().animate({ width: 0, left: '50%' }, 100);
-		$('.dep2').stop().animate({ height: 0 }, 100);
-		$('#menubg').stop().animate({ height: 0 }, 100);
-	}
+function menuopen(e){
+	if (e.type == 'mouseenter')
+		$('header').addClass('_opened');
+	else
+		$('header').removeClass('_opened');
 }
